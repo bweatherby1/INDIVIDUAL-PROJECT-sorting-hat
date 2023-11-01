704d8d2149d012b4feb58b6e3aec7514011108f9
Brad Weatherby <bweatherby1@gmail.com git config --global user.name Brad>
2023-11-01 00:26:36 -0500

    a bunch of stuff

---
+++ main.js
@@ -0,0 +1,82 @@
+const students = [ {
+  id: 1,
+  name: "Larry Popper",
+  mostFun: "Chest Bumps",
+  house:"Griffinsnitch"
+},
+{
+  id: 2,
+  name: "Maco Dalfoy",
+  mostFun: "School Dungeon Raves",
+  house: "Sneakerin"
+},
+{
+id: 3,
+name: "Lucy Lovebad",
+mostFun: "Attending class",
+house: "Whiteclaw"
+},
+{
+  id: 4,
+  name: "Dedric Ciggory",
+  mostFun: "A hard day's work",
+  house: "HuffnPuff"
+}
+]
+
+ 
+const renderToDom = (students) => {
+  let domString = '';
+  for(student of students) {
+    domString += `<div id="studentCards" class="card" style="width: 18rem;">
+    <div class="name"></div>
+    <h5 class="name">${student.name}</h5>
+    <p>${student.house}
+    <br>
+    ${student.mostFun}
+    </p>
+    </div>`
+
+  }
+    
+    const app = document.querySelector("#app")
+    app.innerHTML = domString
+}
+
+renderToDom(students)
+
+const gButton = document.querySelector("#gb")
+ const sButton = document.querySelector("#sb")
+ const wButton = document.querySelector("#wb")
+ const hButton = document.querySelector("#hb")
+ const cButton = document.querySelector(".clortho")
+
+
+const filter = (house) => {
+let allStudentsArray = []
+  for(student of students) {
+    if(student.house === house) {
+      allStudentsArray.push(student)
+   } 
+  } 
+  renderToDom(allStudentsArray)
+} 
+gButton.addEventListener("click", () => {
+filter ("Griffinsnitch");
+})
+sButton.addEventListener("click", () => {
+filter ("Sneakerin");
+})
+wButton.addEventListener("click", () => {
+filter ("Whiteclaw");
+})
+hButton.addEventListener("click", () => {
+  filter ("HuffnPuff");
+})
+
+cButton.addEventListener("click", () => {
+renderToDom(students);
+})
+
+
+startApp()
